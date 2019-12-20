0921f3b7b1ec3ca1ff25d7c501831c7249eac562
Masood Adeli <masood.adeli@cafebazaar.ir>
2019-12-20 20:55:51 +0330

    Adds several tables

--- create_tables.sql
+++ create_tables.sql
@@ -1,23 +1,52 @@
-CREATE DATABASE IF NOT EXISTS 'imdb_db';
+CREATE DATABASE
+IF NOT EXISTS 'imdb_db';
 
-CREATE TABLE User (
-    id int NOT NULL PRIMARY KEY,
-    email_address varchar(255) NOT NULL UNIQUE,
-    password varchar(20),
-    
-    
+CREATE TABLE country
+(
+  id int NOT NULL PRIMARY KEY,
+  name varchar(100) NOT NULL
 );
 
-
-CREATE TABLE Persons (
-  PersonID int,
-  LastName varchar(255),
-  FirstName varchar(255),
-  Address varchar(255),
-  City varchar(255)
+CREATE TABLE movie
+(
+  id int NOT NULL PRIMARY KEY,
+  imdb_code varchar(30) NOT NULL UNIQUE,
+  country_id int FOREIGN KEY REFERENCES country(id),
 );
-CREATE TABLE Orders (
-  OrderID int NOT NULL PRIMARY KEY,
-  OrderNumber int NOT NULL,
-  PersonID int FOREIGN KEY REFERENCES Persons(PersonID)
+
+CREATE TABLE age_group
+(
+  id int NOT NULL PRIMARY KEY,
+  min_age int NOT NULL,
+  max_age int NOT NULL,
+  name varchar(30)
+);
+
+CREATE TABLE movie_suitable_for
+(
+  id int NOT NULL PRIMARY KEY,
+  movie_id int FOREIGN KEY REFERENCES movie(id),
+  age_group_id int FOREIGN KEY REFERENCES age_group(id)
+);
+
+CREATE TABLE critic
+(
+  id int NOT NULL PRIMARY KEY,
+  first_name varchar(100) NOT NULL,
+  last_name varchar(100) NOT NULL
+);
+
+CREATE TABLE critic_reviews
+(
+  id int NOT NULL PRIMARY KEY,
+  movie_id int FOREIGN KEY REFERENCES movie(id),
+  critic_id int FOREIGN KEY REFERENCES critic(id)
+  -- TODO: add score with range
+);
+
+CREATE TABLE user
+(
+  id int NOT NULL PRIMARY KEY,
+  email_address varchar(255) NOT NULL UNIQUE,
+  password varchar(20)
 );
